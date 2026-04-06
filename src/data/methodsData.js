1d4d9508fc701fe55adbe1e2811eaab6d6248a87
art-f-py <7arturfeijopoa@gmail.com>
2026-04-06 15:47:54 -0300

    add bar chart and ranking visualization

---
+++ src/data/methodsData.js
@@ -0,0 +1,5 @@
+export const methodsData = [
+    { method: "OP", score: 78 },
+    { method: "BC", score: 65 },
+    { method: "C&F", score: 82 }
+];
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,8 +1,46 @@
+import { methodsData } from "../data/methodsData"
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid
+} from "recharts";
+
 function Statistics() {
+  //encontra o método com a maior pontuação
+  const bestMethod = methodsData.reduce((prev, current) => 
+    prev.score > current.score ? prev : current);
+
+  //ordena os métodos por score (maior -> menor) para gerar o ranking
+  const sortedMethods = [...methodsData].sort((a, b) => 
+    b.score - a.score);
+
   return (
     <div>
       <h1>MMS 2.0</h1>
       <p>Página de Estatísticas</p>
+
+      {sortedMethods.map((item, index) => (
+        <p key = {item.method}>
+          {index + 1}. {item.method}: {item.score}
+        </p>
+      ))}
+      <h2>Melhor Método: {bestMethod.method} com pontuação {bestMethod.score}</h2>
+
+      <h2>Gráfico de Pontuação</h2>
+      <BarChart
+        width={500}
+        height={300}
+        data={sortedMethods}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="method" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="score" fill="#8884d8" />
+      </BarChart>
     </div>
   );
 }
